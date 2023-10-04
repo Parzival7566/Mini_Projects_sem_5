d92de2349544db86fc6761512c5b747cd53c5434
VedantDeshmukh1 <113693797+VedantDeshmukh1@users.noreply.github.com>
2023-10-04 15:07:11 +0530

    Update app.py

--- flask_mongo_backend/app.py
+++ flask_mongo_backend/app.py
@@ -1,13 +1,55 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
+client = MongoClient("mongodb://localhost:27017/")  # MongoDB connection URL
+db = client['image_db']  # Use or create a database called 'image_db'
+collection = db['images']  # Use or create a collection called 'images'
 
-client = MongoClient('localhost', 27017)
-
-db = client.flask_db
-todos = db.todos
-
-@app.route('/', methods=('GET', 'POST'))
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'image' in request.files:
+        image_file = request.files['image']
+        if image_file.filename != '':
+            # Save the uploaded image to a folder (optional)
+            upload_folder = 'uploads'
+            os.makedirs(upload_folder, exist_ok=True)
+            image_path = os.path.join(upload_folder, image_file.filename)
+            image_file.save(image_path)
+
+            # Insert the image file path into MongoDB
+            image_data = {'path': image_path}
+            collection.insert_one(image_data)
+
+            return jsonify(message='Image uploaded successfully.')
+
+    return jsonify(message='No image uploaded.')
+
+@app.route('/upload_webcam', methods=['POST'])
+def upload_webcam_capture():
+    if 'webcam_image' in request.files:
+        webcam_image = request.files['webcam_image']
+        if webcam_image.filename != '':
+            # Save the webcam capture to the 'uploads' folder
+            upload_folder = 'uploads'
+            os.makedirs(upload_folder, exist_ok=True)
+            image_path = os.path.join(upload_folder, webcam_image.filename)
+            webcam_image.save(image_path)
+
+            # Insert the webcam capture into MongoDB as a binary file
+            with open(image_path, 'rb') as image_file:
+                image_data = {'webcam_capture': image_file.read()}
+                collection.insert_one(image_data)
+
+            return jsonify(message='Webcam capture saved successfully.')
+
+    return jsonify(message='No webcam capture available.')
+
+
+if __name__ == '__main__':
+    app.run()

f38cf9d7e117aca34a19e79c88c7b5bd962b2c7f
VedantDeshmukh1 <113693797+VedantDeshmukh1@users.noreply.github.com>
2023-11-20 14:31:37 +0530

    Update app.py

--- Free_Raj/app.py
+++ Free_Raj/app.py
@@ -1,7 +1,11 @@
+import Recommendation_sample_new
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from datetime import datetime
 import webbrowser
+import hashlib
+#from flask import jsonify
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "SECRET_KEY"
@@ -14,6 +18,7 @@ students_collection = mongo.db["students"]
 menu_collection = mongo.db["menu"]
 orders_collection = mongo.db["orders"]
 cart=mongo.db["cart"]
+past_orders_collection=mongo.db["past_orders"]
 vendor_collection=mongo.db["vendor"]
 
 vendor_collection.drop()
@@ -24,27 +29,7 @@ vendor_data = [
 
 vendor_collection.insert_many(vendor_data)
 
-# Sample menu data
-'''sample_menu = [
-    {"name": "Item 1", "preparation_time": "10 minutes"},
-    {"name": "Item 2", "preparation_time": "15 minutes"},
-    {"name": "Item 3", "preparation_time": "12 minutes"},
-    {"name": "Item 4", "preparation_time": "8 minutes"},
-    {"name": "Item 5", "preparation_time": "20 minutes"},
-    {"name": "Item 6", "preparation_time": "7 minutes"},
-    {"name": "Item 7", "preparation_time": "25 minutes"},
-    {"name": "Item 8", "preparation_time": "11 minutes"},
-    {"name": "Item 9", "preparation_time": "14 minutes"},
-    {"name": "Item 10", "preparation_time": "18 minutes"},
-]
 
-# Automatically populate the menu data when the app starts
-if menu_collection.count_documents({}) == 0:
-    # Remove existing menu items
-    menu_collection.delete_many({})
-    # Populate the menu data
-    menu_collection.insert_many(sample_menu)
-'''
 @app.route("/vendor_login", methods=["GET", "POST"])
 def vendor_login():
     if request.method == "POST":
@@ -63,24 +48,86 @@ def vendor_login():
     
     return render_template("vendor_login.html")
 
+
 @app.route("/vendor_dashboard")
 def vendor_dashboard():
+    # Retrieve current orders
+    current_orders = list(orders_collection.find({"status": "open"}))
+    
+    # Retrieve completed orders with a status of "closed"
+    completed_orders = list(orders_collection.find({"status": "completed"}))
+
+    # Retrieve completed orders with a status of "closed"
+    collected_orders = list(orders_collection.find({"status": "collected"}))
+
     # Retrieve analytics data
     total_orders = orders_collection.count_documents({})
     # You can calculate 'most_ordered_item' here
-
-    # Retrieve current and completed orders
-    current_orders = orders_collection.find({"status": "open"})
-    completed_orders = orders_collection.find({"status": "completed"})
     menu_items = list(menu_collection.find())
     # Calculate earnings
     # You can calculate 'total_earnings' here
+    
+    # Calculate total price of all orders
+    total_price = sum(int(order["price"])*int(order["quantity"]) for order in current_orders + completed_orders + collected_orders)
 
-    return render_template("vendor_dashboard.html", total_orders=total_orders,
-                           menu_items=menu_items,
-                          # most_ordered_item=most_ordered_item, current_orders=current_orders,
-                           #completed_orders=completed_orders, total_earnings=total_earnings)
-    )
+    return render_template("vendor_dashboard.html",
+                           menu_items=menu_items, collected_orders=collected_orders, current_orders=current_orders, completed_orders=completed_orders, total_orders=total_orders,
+                           total_price=total_price)
+
+
+
+@app.route("/mark_order_done", methods=["POST"])
+def mark_order_done():
+    if request.method == "POST":
+        order_id = request.form.get("order_id")
+
+        # Find the order in the 'orders_collection' by its ObjectId
+        order = orders_collection.find_one({"_id": ObjectId(order_id)})
+
+        if order:
+            # Update the status to "completed"
+            orders_collection.update_one({"_id": ObjectId(order_id)}, {"$set": {"status": "completed"}})
+
+            # Redirect back to the vendor dashboard
+            return redirect(url_for("vendor_dashboard"))
+
+    # Handle errors or invalid requests
+    return "Invalid request or order not found"
+
+
+@app.route("/mark_order_collected", methods=["POST"])
+def mark_order_collected():
+    if request.method == "POST":
+        order_id = request.form.get("order_id")
+
+        # Find the order in the 'orders_collection' by its ObjectId
+        order = orders_collection.find_one({"_id": ObjectId(order_id)})
+
+        if order:
+            # Check if the order is already collected
+            if order.get("status") == "collected":
+                return "Order is already collected"
+            
+            # Update the status to "collected"
+            orders_collection.update_one({"_id": ObjectId(order_id)}, {"$set": {"status": "collected"}})
+
+            order_data = {
+            "order_id": str(order["_id"]),
+            "items": order["item"],
+            "quantities": order["quantity"],
+            "prices": order["price"],
+            "status": order["status"],
+            "order_cost": sum(int(price) * int(quantity) for price, quantity in zip(order["price"], order["quantity"]))
+            }
+            # Add the order to the 'past_orders_collection' collection
+            past_orders_collection.insert_one(order_data)
+
+            # Redirect back to the vendor dashboard
+            return redirect(url_for("vendor_dashboard"))
+        else:
+            error_message = "Order not found."
+            return render_template("vendor_dashboard.html", error=error_message)
+        
 
 @app.route("/edit_menu", methods=["GET", "POST"])
 def edit_menu():
@@ -104,6 +151,7 @@ def edit_menu():
 
     return render_template("edit_menu.html")
 
+
 @app.route("/delete_menu_item/<menu_item_id>", methods=["POST"])
 def delete_menu_item(menu_item_id):
     # Convert menu_item_id to ObjectId
@@ -114,6 +162,7 @@ def delete_menu_item(menu_item_id):
 
     # Redirect back to the vendor dashboard or another appropriate page
     return redirect(url_for("vendor_dashboard"))
+
 
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -127,10 +176,11 @@ def login():
             return render_template("student_login.html", error=error_message)
     return render_template("student_login.html")
 
+
 @app.route("/create_account", methods=["GET", "POST"])
 def create_account():
     if request.method == "POST":
-        uname=request.form["uname"]
+        uname = request.form["uname"]
         prn = request.form["prn"]
         password = request.form["password"]
         student = students_collection.find_one({"prn": prn})
@@ -138,49 +188,137 @@ def create_account():
             error_message = "Account already exists for PRN. Please try again."
             return render_template("create_account.html", error=error_message)
         else:
+            # Hash the password using SHA-256
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
             student_data = {
                 "uname": uname, 
                 "prn": prn,
-                "password": password
+                "password": hashed_password,
+                "order_number": 1
             }
             students_collection.insert_one(student_data)
             return redirect(url_for("login"))
     return render_template("create_account.html")
+
 
 @app.route("/dashboard/<prn>", methods=["GET", "POST"])
 def dashboard(prn):
     student = students_collection.find_one({"prn": prn})
     if student:
         menu = list(menu_collection.find())  # Fetch menu items from MongoDB
-        return render_template("studentdashboard.html", student=student, menu=menu)
+        closed_orders = list(orders_collection.find({"student_prn": prn, "status": "completed"}))  # Fetch closed orders for the student
+        if closed_orders == []:
+            return render_template("studentdashboard.html", student=student, menu=menu)
+        else:
+            return render_template("studentdashboard.html", student=student, menu=menu, closed_orders=closed_orders)
     else:
         error_message = "Student data not found."
         return render_template("student_login.html", error=error_message)
+    
+@app.route("/recommendations")
+def show_recommendations():
+    username = request.args.get("username")
+    recent_item = request.args.get("recent_item")
 
+    # Your existing logic to fetch recommendations from the database
+    recommendations = Recommendation_sample_new.recommend(recent_item)
+
+    return render_template("recommendations.html", username=username, recent_item=recent_item, recommendations=recommendations)
+
+
+
+
+
+
+menu_items = [menu_item["name"] for menu_item in menu_collection.find()]
+
+
+ # Add this route for recommendations
+@app.route("/recommendations/<prn>")
+def get_recommendation(prn):
+    # Fetch the most recent order for the student based on order_time
+    most_recent_order = orders_collection.find_one(
+        {"student_prn": prn},
+        sort=[("order_time", -1)]
+    )
+
+    # Extract the most recent item from the order
+    recent_item = most_recent_order.get("item") if most_recent_order else None
+
+    # Call the recommend function with the most recent item name
+    recommendations = Recommendation_sample_new.recommend(recent_item)
+
+    # Filter recommendations to include only items present in the menu
+    filtered_recommendations = [item for item in recommendations if item in menu_items]
+
+    # Render the recommendations.html template with the relevant data
+    return render_template("recommendations.html", username=prn, recent_item=recent_item, recommendations=filtered_recommendations)
+
+    
+    
+@app.route("/past_orders/<prn>")
+def past_orders(prn):
+    # Group orders by order number and sort each group by order_time in descending order
+    past_orders = orders_collection.aggregate([
+        {"$match": {"student_prn": prn}},
+        {"$group": {
+            "_id": "$order_number",
+            "orders": {
+                "$push": {
+                    "item": "$item",
+                    "quantity": "$quantity",
+                    "price": "$price",
+                    "status": "$status",
+                    "order_cost": "$order_cost",
+                    "order_time": "$order_time"
+                }
+            }
+        }},
+        {"$project": {
+            "order_number": "$_id",
+            "orders": {"$slice": ["$orders", 1]},  # Take only the most recent order
+            "_id": 0
+        }},
+        {"$sort": {"orders.0.order_time": -1}}  # Sort by order_time in descending order
+    ])
+
+    return render_template("past_orders.html", past_orders=past_orders)
+
+def get_most_recent_item(past_orders):
+    if past_orders:
+        most_recent_order = past_orders[0]
+        return most_recent_order.get("item")  # Replace with the actual key for the item name
+    else:
+        return None  # Handle the case when there are no past orders
+        
 @app.route("/place_order", methods=["POST"])
 def place_order():
-    prn = request.form["prn"]
-    student = students_collection.find_one({"prn": prn})
-    if student:
-        items = []
-        quantities = []
-        preparation_times = []
+    if request.method == "POST":
+        student_prn = request.form.get("prn")
+        items = request.form.getlist("item")
 
-        for item, quantity, preparation_time in zip(request.form.getlist("item"), request.form.getlist("quantity"), request.form.getlist("preparation_time")):
-            if int(quantity) > 0:
-                items.append(item)
-                quantities.append(quantity)
-                preparation_times.append(preparation_time)
+        for item in items:
+            quantity = request.form.get(f"quantity_{item}")
+            preparation_time = request.form.get(f"preparation_time_{item}")
+            price = request.form.get(f"price_{item}")
 
-        cart_data = {
-            "student_prn": prn,
-            "items": items,
-            "quantities": quantities,
-            "preparation_times": preparation_times
-        }
-        cart.insert_one(cart_data)
+            if quantity != '0':
+                cart_item = {
+                    "student_prn": student_prn,
+                    "item": item,
+                    "quantity": quantity,
+                    "preparation_time": preparation_time,
+                    "price": price,
+                    "order_time": datetime.now()  # Add the current date and time
+                }
+                cart.insert_one(cart_item)
 
-    return redirect(url_for("dashboard", prn=prn))
+        flash("Item(s) added to cart successfully!")
+        if student_prn:
+            return redirect(url_for("dashboard", prn=student_prn))
+
+
 
 @app.route("/view_cart", methods=["GET", "POST"])
 def view_cart():
@@ -193,11 +331,15 @@ def view_cart():
             return redirect(url_for("view_cart", prn=prn))
         else:
             cart_items = list(cart.find({"student_prn": prn}))
-            return render_template("view_cart.html", student=student, cart_items=cart_items)
+            if cart_items ==[]:
+                return render_template("view_cart.html", student=student)
+            else:
+                return render_template("view_cart.html", student=student, cart_items=cart_items)
     else:
         error_message = "Student data not found."
         return render_template("student_login.html", error=error_message)
     
+
 @app.route("/payment_gateway", methods=["GET", "POST"])
 def payment_gateway():
     prn = request.args.get("prn")
@@ -211,22 +353,27 @@ def payment_gateway():
             
             for item in cart_items:
                 item["status"] = "open"
+                item["order_time"] = datetime.now()
+                item["order_number"] = student["order_number"]
                 orders_collection.insert_one(item)
             cart.delete_many({"student_prn": prn})
+            students_collection.update_one({"prn": prn}, {"$inc": {"order_number": 1}})
             return redirect(url_for("dashboard", prn=prn))
         else:
             items = []
             quantities = []
             preparation_times = []
+            prices = []
             for cart_item in cart_items:
-                for index in range(len(cart_item["items"])):
-                    items.append(cart_item["items"][index])
-                    quantities.append(cart_item["quantities"][index])
-                    preparation_times.append(cart_item["preparation_times"][index])
+                items.append(cart_item["item"])
+                quantities.append(cart_item["quantity"])
+                preparation_times.append(cart_item["preparation_time"])
+                prices.append(cart_item["price"])
             return render_template("payment_gateway.html", student=student, cart_items=cart_items, items=items, quantities=quantities, preparation_times=preparation_times)
     else:
         error_message = "Student data not found."
         return render_template("student_login.html", error=error_message)
+    
 
 if __name__ == "__main__":
     webbrowser.open('http://127.0.0.1:5000/')
